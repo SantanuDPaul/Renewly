6b0a23b5d689c12bd2551ca312ba158c6922eb84
Santanu Paul <santanupaul366@gmail.com>
2026-07-13 12:05:25 +0530

    Implement user registration feature

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib import messages
+
+from django.shortcuts import redirect, render
+from django.views import View
+
+from accounts.forms import RegistrationForm
 
 # Create your views here.
+class RegisterView(View):
+    def get(self, request):
+        form = RegistrationForm()
+        return render(request, 'accounts/register.html', {'form': form})
+
+    def post(self, request):
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration successful. You can now log in.')
+            return redirect('register')  # Redirect to login page after successful registration
+        return render(request, 'accounts/register.html', {'form': form})
